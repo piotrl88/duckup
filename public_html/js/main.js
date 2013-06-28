965e31852bb8211e34c1db9b41306aef5ffe1094
marcin <poczta.maciejewski@gmail.com>
2013-06-28 23:21:43 +0200

    scope issues fixed in model, init, enemy methods

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -2,25 +2,35 @@ var DuckUp = {
     model : {
         objSize : 32,
         gameCanvas : document.getElementById('canvas'),
-//        gameContext : DuckUp.model.gameCanvas.getContext('2d'),
-//        canvasX : (DuckUp.model.gameCanvas.width / 2) - objSize,
-//        canvasY : (DuckUp.model.gameCanvas.height / 2) - objSize
+        gameContext: '',
+        canvasX: '',
+        canvasY: ''
     },
     init: function() {
         //window.addEventListener('keydown', move, true);
-        this.enemy();        
+        //this.enemy();
+        this.model.gameContext = this.model.gameCanvas.getContext('2d');
+        this.model.canvasX = (this.model.gameCanvas.width / 2) - this.model.objSize;
+        this.model.canvasY = (this.model.gameCanvas.height / 2) - this.model.objSize;
     },
     enemy: function() {
         var enemyImg = new Image();
         enemyImg.onload = function() {
-            //this.model.gameContext.drawImage(enemyImg, this.model.canvasX, this.model.canvasY, this.model.objSize, this.model.objSize);
+            var model = that.model;
+            model.gameContext.drawImage(
+                enemyImg,
+                model.canvasX,
+                model.canvasY,
+                model.objSize,
+                model.objSize
+            );
         };
         enemyImg.src = 'img/fox.png';
     },
     player: function() {
         var playerImg = new Image();
         playerImg.onload = function() {
-            //this.model.gameContext.drawImage(playerImg, 0, 0, this.model.objSize, this.model.objSize);
+            this.model.gameContext.drawImage(playerImg, 0, 0, this.model.objSize, this.model.objSize);
         };
         playerImg.src = 'img/duck.png';
 
